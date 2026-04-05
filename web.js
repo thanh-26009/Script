7ece6e56cd8fe37c54b866e316cb0764ad281a15
thanh2009 <tempmail2k9@gmail.com>
2026-04-05 20:57:31 +0700

    Update web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@
 
     if (beforeCom === "damconuong" && !document.cookie.includes("dam_co_nuong_session=")) {
 
-        document.cookie = "dam_co_nuong_session=eyJpdiI6IjF0WDN6bzVsVHRNOW94dHRFRGNSeXc9PSIsInZhbHVlIjoiZllESWF2NGdoc1BiVEFMejlhSGRiakNEUmJHRlNGcTNMbG5tMWdGN09JY1g1aE9mZWFqSERrYk4xRFp3UDBxTFFSd25qS3RvbkhGQlBDM2xNajY0TFVpUG5zcDdPK01XN2w4Qk81NVFBODAwQU9ucDhBOXBRa2VRNVNVWStMSGkiLCJtYWMiOiI2NGFmZmNhNThlMjMzN2JkY2M3MTAxZTBmMWYzOGE1NzY1YmY3YjMyZTQ1OTE3MjJkZGU3Nzg2ZWYxMDMxNGIzIiwidGFnIjoiIn0%3D; path=/";
+        document.cookie = "dam_co_nuong_session=eyJpdiI6IjF0WDN6bzVsVHRNOW94dHRFRGNSeXc9PSIsInZhbHVlIjoiZllESWF2NGdoc1BiVEFMejlhSGRiakNEUmJHRlNGcTNMbG5tMWdGN09JY1g1aE9mZWFqSERrYk4xRFp3UDBxTFFSd25qS3RvbkhGQlBDM2xNajY0TFVpUG5zcDdPK01XN2w4Qk81NVFBODAwQU9ucDhBOXBRa2VRNVNVWStMSGkiLCJtYWMiOiI2NGFmZmNhNThlMjMzN2JkY2M3MTAxZTBmMWYzOGE1NzY1YmY3YjMyZTQ1OTE3MjJkZGU3Nzg2ZWYxMDMxNGIzIiwidGFnIjoiIn0=; path=/";
 
         location.reload();
     }
